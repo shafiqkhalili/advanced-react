a7a3fbf5695b3cfc475997a30cf4d2e677c697d2
Shafigh Khalili <shafiqkhalili@gmail.com>
2022-08-24 15:07:41 +0200

    Add styles and GlobalStyles

--- sick-fits/frontend/pages/_document.js
+++ sick-fits/frontend/pages/_document.js
@@ -1,9 +1,20 @@
-import Document, { Html, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+    const page = renderPage(
+      (App) => (props) => sheet.collectStyles(<App {...props} />)
+    );
+    const styleTags = sheet.getStyleElement();
+    return { ...page, styleTags };
+  }
+
   render() {
     return (
       <Html lang="en-us">
+        <Head />
         <body>
           <Main />
           <NextScript />
