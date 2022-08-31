a095bf87332f7dc7c4f654316a130c2f5c69a932
Shafigh Khalili <shafiqkhalili@gmail.com>
2022-08-31 11:20:59 +0200

    More fields for productImage

--- sick-fits/backend/schemas/ProductImage.ts
+++ sick-fits/backend/schemas/ProductImage.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config';
-import { text } from '@keystone-next/fields';
+import { text, relationship } from '@keystone-next/fields';
 import { cloudinaryImage } from '@keystone-next/cloudinary';
 import { list } from '@keystone-next/keystone/schema';
 
@@ -17,5 +17,11 @@ export const ProductImage = list({
       label: 'Source',
     }),
     altText: text(),
+    product: relationship({ ref: 'Product.photo' }),
+  },
+  ui: {
+    listView: {
+      initialColumns: ['image', 'altText', 'product'],
+    },
   },
 });
